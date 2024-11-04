2a6669a4aa3ee1948616f81df78a817351c4ec78
dimassans <dimastrisaputra0868@gmail.com>
2024-11-04 17:01:08 +0700

    update tampilan

--- resources/js/components/PortfolioComponent.vue
+++ resources/js/components/PortfolioComponent.vue
@@ -1,20 +1,26 @@
 <template>
-    <nav class="navbar navbar-expand-lg shadow-sm bg-body-tertiary rounded sticky-top">
-        <div class="container-fluid">
-            <img src="/assets/img/logo SNS HD.png" alt="Bootstrap" width="200px" height="64px" class="my-2" style="margin-left: 56px;">
-            <div class="collapse navbar-collapse d-flex justify-content-end mx-4" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link mx-4 text-info" aria-current="page" href="#">Home</a>
-                    <a class="nav-link mx-4 text-info" href="#">About Us</a>
-                    <a class="nav-link mx-4 text-info" href="#">Career</a>
-                    <a class="nav-link mx-4 text-info" aria-disabled="true">Features</a>
-                </div>
+    <nav class="navbar navbar-expand-lg shadow-sm bg-white sticky-top">
+        <div class="container d-flex justify-content-evenly">
+            <img src="/assets/img/logo SNS HD.png" alt="Bootstrap" width="140px" class="my-2">
+            <a class="nav-link mx-3 text-info" aria-current="page" href="#">Product</a>
+            <a class="nav-link mx-3 text-info" href="#">About Us</a>
+            <a class="nav-link mx-3 text-info" href="#">Products</a>
+            <a class="nav-link mx-3 text-info" aria-disabled="true">Services</a>
+            <a class="nav-link mx-3 text-info" aria-disabled="true">Careers</a>
+            <a class="nav-link mx-3 text-info" aria-disabled="true">Contact Us</a>
+            <!-- Example single danger button -->
+            <div class="ms-2">
+            <select class="form-select" aria-label="Default select example" style="border:none;">
+                <option selected>Indonesia</option>
+                <option value="1">English</option>
+            </select>
             </div>
         </div>
     </nav>
+    <!--
     <section id="hero" class="hero">
 
-        <img src="assets/img/hero-bg-abstract1.png" class="color-section" alt="background">
+        <img src="assets/img/hero-bg-abstract1.png" alt="background">
   
         <div class="container">
           <div class="row justify-content-center">
@@ -29,10 +35,28 @@
             <a href="#" class="ms-5 btn-get-ourstory" style="text-decoration:none;">
               <i class="bi bi-play-fill" style="font-size: 16px;"></i> Our Story</a>
           </div>
-          <div class="text-center">
-            <div class="my-4"><h1>500</h1></div>
-          </div>
-  
+          <div class="d-flex justify-content-center">
+            <div class="row row-cols-lg-4">
+                <div class="col me-4">
+                        <h1 class="text-cente mt-4">500+</h1>
+                        <h1 class="fs-3 text-center" >User</h1>
+                </div>
+                <div class="col ms-4 me-4">
+                    <div class="text-center ps-2 my-4">
+                        <h1>20+</h1>
+                        <h1 class="fs-3 text-center">positions</h1>
+                    </div>
+                </div>
+                <div class="col ms-4">
+                    <div class="text-center my-4">
+                        <h1>200+</h1>
+                        <h1 class="fs-3 text-center">Partners</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>-->
+        <!--
           <div class="row gy-4 mt-5">
             <div class="col-md-6 col-lg-3">
               <div class="icon-box">
@@ -40,7 +64,7 @@
                 <h4 class="title"><a href="">Lorem Ipsum</a></h4>
                 <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
               </div>
-            </div><!--End Icon Box -->
+            </div>
   
             <div class="col-md-6 col-lg-3">
               <div class="icon-box">
@@ -48,7 +72,7 @@
                 <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
                 <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
               </div>
-            </div><!--End Icon Box -->
+            </div>
   
             <div class="col-md-6 col-lg-3">
               <div class="icon-box">
@@ -56,7 +80,7 @@
                 <h4 class="title"><a href="">Magni Dolores</a></h4>
                 <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
               </div>
-            </div><!--End Icon Box -->
+            </div>
   
             <div class="col-md-6 col-lg-3">
               <div class="icon-box">
@@ -64,12 +88,94 @@
                 <h4 class="title"><a href="">Nemo Enim</a></h4>
                 <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
               </div>
-            </div><!--End Icon Box -->
+            </div>
   
           </div>
         </div>
   
-      </section>
+    </section>-->
+    <div class="hero">
+        <div class="container mb-4">
+            <div class="row">
+                <div class="col-md-6 col-lg-6" style="margin-top: 128px; margin-bottom: 128px;">
+                    <h1 style="color: #00AEEF; font-size: 64px;">Cooling Care on the Go, AC Pro You Should Know</h1>
+                    <div class="row">
+                        <div class="col-md-5 col-lg-5">
+                        <p class="description" style="font-size: 12px;">Unlock Cool Comfort and Superior AC Solutions with <span style="color: blue;"><b>SNS</b></span> : Ride in Style!</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 col-lg-4">
+                            <a href="#about" class="btn-get-started" style="text-decoration:none;">Our Service</a>
+                        </div>
+                        <div class="col-md-5 col-lg-5">
+                            <a href="#about" class="btn-get-started" style="text-decoration:none;">Our Products</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-6">
+                    <img src="assets/img/illus.png" style="width: 700px;" alt="...">
+                </div>
+            </div>
+        </div>
+    </div>
+        <div class="hero">
+            <div class="container">
+                <div class="row mb-5">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body rounded px-4" style="background-color: #0081B1;">
+                                <div class="row">
+                                    <div class="col-md-10 col-lg-6">
+                                    <h1 style="color:white; font-size: 40px;">Tentang SNS.AC</h1>
+                                    <div class="row">
+                                        <div class="col">
+                                            <p class="description" style="color:white; font-size: 16px;">SNS.AC adalah perusahaan SNS (PT. Surya Nusantara Sentosa) yang bergerak dibidang jasa service AC Mobil & Service AC Mobil dan pendirian perusahaan. Sejak 2017, SNS.AC telah membantu lebih dari 8000 costumer dalam segi otomotif di Indonesia.</p>
+                                            <p class="description" style="color:white; font-size: 16px;"><i class="bi bi-play-fill">Sekilas tentang SNS.AC</i></p>
+                                        </div>
+                                    </div>
+                                    <div class="row mx-5">
+                                        <div class="col-md-4 text-center">
+                                            <h1 class="fs-1" style="color:white;">7</h1>
+                                            <p style="font-size: 16px; color:white;"><b>Tahun Pengalaman</b></p>
+                                        </div>
+                                        <div class="col-md-3 text-center">
+                                            <h1 class="fs-1" style="color:white;">20+</h1>
+                                            <p style="font-size: 16px; color:white;"><b>Lokasi Dealer</b></p>
+                                        </div>
+                                        <div class="col-md-3 text-center">
+                                            <h1 class="fs-1" style="color:white;">200+</h1>
+                                            <p style="font-size: 16px; color:white;"><b>Cabang</b></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <img src="assets/img/Reta.png" style="width: 495px; margin-left:58%" alt="...">
+                    </div>   
+                </div>
+                <div class="container text-center">
+                    <div class="row justify-content-center">
+                        <div class="col-md-10 mt-5">
+                            <h1 class="fs-1">Perawatan mobil berkualitas kini semakin mudah dan praktis</h1>
+                            <p class="px-5 mb-5">Kepuasan pelanggan adalah prioritas kami. Oleh karena itu, kami berusaha memberikan layanan perawatan mobil yang sederhana, nyaman, dan terjangkau.</p>
+                            <a class="fs-4" style="text-decoration:none;" href="">Discover our Services <i class="bi bi-chevron-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-md-10 mt-5">
+                            <h1 class="fs-1">Perawatan mobil berkualitas kini semakin mudah dan praktis</h1>
+                            <p class="px-5 mb-5">Kepuasan pelanggan adalah prioritas kami. Oleh karena itu, kami berusaha memberikan layanan perawatan mobil yang sederhana, nyaman, dan terjangkau.</p>
+                            <a class="fs-4" style="text-decoration:none;" href="">Discover our Services <i class="bi bi-chevron-right"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 </template>
 
 <script>
