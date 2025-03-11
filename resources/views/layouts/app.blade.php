364cd7d3e49f15be9a9fc6aeb60c70b97e2baac4
dimassans <dimastrisaputra0868@gmail.com>
2025-03-11 13:25:00 +0700

    fix fav icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
   <!-- Favicons -->
   <link href="assets/img/logo1.png" rel="icon">
-  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link href="assets/img/logo1.png" rel="apple-touch-icon">
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.3/font/bootstrap-icons.min.css" rel="stylesheet">
 
